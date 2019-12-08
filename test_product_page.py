1e0f9d0f1e8dd478ac71a48419d227f06dfdebbb
GulfinaF <gulfinaf@gmail.com>
2019-12-08 20:51:45 +0300

    check empty basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 import time
 import math
 import pytest
@@ -28,13 +29,25 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket_button()
     page.solve_quiz_and_get_code()
     page.should_disappear_success_message()
+
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_message_about_empty_basket()
+    page.should_not_be_items_in_basket()
