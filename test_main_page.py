8ef512e24ba6f44ad8817bcc08222bfa151084be
GulfinaF <gulfinaf@gmail.com>
2019-12-08 20:45:21 +0300

    check empty basket

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,10 @@
 from pages.main_page import MainPage
-from .pages.login_page import LoginPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+from pages.product_page import ProductPage
+import pytest
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -8,13 +12,13 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-    
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+@pytest.mark.skip
 def test_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
@@ -22,3 +26,19 @@ def test_login_page(browser):
     page.should_be_login_url()
     page.should_be_login_form()
     page.should_be_register_form()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_message_about_empty_basket()
+    page.should_not_be_items_in_basket()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_message_about_empty_basket()
+    page.should_not_be_items_in_basket()
