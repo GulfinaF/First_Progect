79ed4bcd0aeac4829e16fe2dd41b676ac5ebe6cb
GulfinaF <gulfinaf@gmail.com>
2019-12-08 19:41:17 +0300

    added simple tests product page

---
+++ test_product_page.py
@@ -0,0 +1,40 @@
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.product_page import ProductPage
+import time
+import math
+import pytest
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket_button()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket_button()
+    page.solve_quiz_and_get_code()
+    page.should_disappear_success_message()
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
