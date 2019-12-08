898fec5a04eb791687b22361cf0076a3d750af3d
GulfinaF <gulfinaf@gmail.com>
2019-12-08 19:38:14 +0300

    added POM structure

---
+++ pages/locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-lg:nth-child(3)")
+    PRODUCT_NAME_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert:nth-child(1) > div:nth-child(2) > strong:nth-child(1)")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6:nth-child(2) > h1:nth-child(1)")
+    PRODUCT_PRICE_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert:nth-child(3) > div:nth-child(2) > p:nth-child(1) > strong:nth-child(1)")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color:nth-child(2)")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR,"div.alert:nth-child(1)")
+
---
+++ pages/product_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from selenium.common.exceptions import NoSuchElementException
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket_button(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
+
+    def should_be_add_to_basket_message(self):
+        added_product_allert= self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_SUCCESS_MESSAGE)
+        product_name=self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        assert  product_name.text == added_product_allert.text , "This product didnt add to basket"
+
+    def should_be_correct_price(self):
+        added_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_SUCCESS_MESSAGE)
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        assert  product_price.text == added_product_price.text , "Prices are different"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented"
