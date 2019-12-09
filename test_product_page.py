a5022d067ea475e3842dd851814bf8c0ea6845ec
GulfinaF <gulfinaf@gmail.com>
2019-12-09 20:47:59 +0300

    added test-class: TestUserAddToBasketFromProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,9 @@ import time
 import math
 import pytest
 
+from conftest import browser
+
+
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
@@ -51,3 +54,25 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket()
     page.should_be_message_about_empty_basket()
     page.should_not_be_items_in_basket()
+@pytest.mark.registerANDadd
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, self.link)
+        page.open()
+        page.register_new_user()
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/visual-guide-to-lock-picking_206/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/visual-guide-to-lock-picking_206/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket_button()
+        page.should_be_add_to_basket_message()
