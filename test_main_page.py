b5a2f9023d74a580e87f930284bd9b07f68e8ef3
GulfinaF <gulfinaf@gmail.com>
2019-12-10 21:18:39 +0300

    add test for all products list

--- test_main_page.py
+++ test_main_page.py
@@ -4,20 +4,21 @@ from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
 import pytest
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 @pytest.mark.skip
 def test_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -26,7 +27,7 @@ def test_login_page(browser):
     page.should_be_login_url()
     page.should_be_login_form()
     page.should_be_register_form()
-
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = BasketPage(browser, link)
@@ -34,11 +35,12 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket()
     page.should_be_message_about_empty_basket()
     page.should_not_be_items_in_basket()
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
-    page = BasketPage(browser, link)
+@pytest.mark.need_review_custom_scenarios
+def test_guest_can_add_3_product_from_all_products_list(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.go_to_basket()
-    page.should_be_message_about_empty_basket()
-    page.should_not_be_items_in_basket()
+    page.go_to_all_products_list()
+    page.add_to_basket_3_product_on_page_button()
+    page.should_not_be_success_message()
+
