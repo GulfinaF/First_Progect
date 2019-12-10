e1d1913d11c808368e328d7ac206e5b575d4d39e
GulfinaF <gulfinaf@gmail.com>
2019-12-10 21:13:52 +0300

    added add to / delete from basket tests

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,4 +1,5 @@
 from .locators import BasketPageLocators
+from .locators import ProductPageLocators
 from .base_page import BasePage
 
 class BasketPage(BasePage):
@@ -10,3 +11,15 @@ class BasketPage(BasePage):
     def should_be_message_about_empty_basket(self):
         assert self.is_element_present(*BasketPageLocators.EMPTY_BACKET_MESSAGE), \
                "There is no message about empty basket"
+
+    def add_to_basket(self):
+        basket_button = self.browser.find_element(*BasketPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
+        go_to_basket_button = self.browser.find_element(*BasketPageLocators.GO_TO_BASKET_BUTTON_AFTER_SUCCESS_MESSAGE)
+        go_to_basket_button.click()
+
+    def delete_item_from_basket(self):
+        sum_of_items_befor_delete = self.browser.find_elements(*BasketPageLocators.BASKET_ITEMS)
+        first_delete_button = self.browser.find_element(*BasketPageLocators.DELETE_FIRST_ITEM_BUTTON)
+        sum_of_items_after_delete = self.browser.find_elements(*BasketPageLocators.BASKET_ITEMS)
+        assert (len(sum_of_items_befor_delete) - 1)==(sum_of_items_after_delete), "Item is not deleted"
