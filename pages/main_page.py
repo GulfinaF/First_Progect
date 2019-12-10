b55e365757b617370022f9cc48f2bed2bde7ea70
GulfinaF <gulfinaf@gmail.com>
2019-12-10 21:17:15 +0300

    add def for all products list

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,3 +8,12 @@ class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
 
+    def go_to_all_products_list(self):
+        all_products_btn=self.browser.find_element(*MainPageLocators.ALL_PRODUCTS_BUTTON)
+        all_products_btn.click()
+    def add_to_basket_3_product_on_page_button(self):
+        basket_button = self.browser.find_element(*MainPageLocators. ADD_TO_BASKET_BUTTON_3_PRODUCT_ON_PAGE)
+        basket_button.click()
+    def should_not_be_success_message(self):
+        assert self.is_element_present(*MainPageLocators.SUCCESS_MESSAGE), \
+           "Success message is not presented"
